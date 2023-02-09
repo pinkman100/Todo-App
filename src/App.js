685082a374bf130956d3bf07713900d5a7f932c7
unknown <muhammedfaizan1999@gmail.com>
2023-02-09 11:12:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from "react";
+import Todo from './components/Todoapp/Todo';
+
+function App() {
+  return <div className="App">
+    <Todo/>
+  </div>;
+      
+}
+
+export default App;
